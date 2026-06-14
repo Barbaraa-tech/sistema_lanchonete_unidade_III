b692b2364eb3f1578d5b660162d7c2e08b07f84d
Barbaraa-tech <barbara.jesus@alunos.ufersa.edu.br>
2026-06-13 23:11:23 -0300

    Update produtos.c

--- produtos.c
+++ produtos.c
@@ -1,114 +1,21 @@
-#include <stdio.h>
-#include <string.h>
-#include "produtos.h"
+#ifndef PRODUTOS_H
+#define PRODUTOS_H
 
-void adicionarProduto(Produto cardapio[], int *totalProdutos)
+#define MAX 100
+
+typedef struct
 {
-    if (*totalProdutos >= MAX)
-    {
-        printf("Limite de produtos atingido.\n");
-        return;
-    }
+    char nome[50];
+    float preco;
+} Produto;
 
-    printf("Digite o nome do produto: ");
-    scanf(" %[^\n]", cardapio[*totalProdutos].nome);
+/* Funções do sistema */
+void adicionarProduto(Produto cardapio[], int *totalProdutos);
 
-    printf("Digite o preco do produto: ");
-    scanf("%f", &cardapio[*totalProdutos].preco);
+void listarProdutos(Produto cardapio[], int totalProdutos);
 
-    if (cardapio[*totalProdutos].preco < 0)
-    {
-        printf("Preco invalido!\n");
-        return;
-    }
+void atualizarProduto(Produto cardapio[], int totalProdutos);
 
-    (*totalProdutos)++;
+void removerProduto(Produto cardapio[], int *totalProdutos);
 
-    printf("Produto cadastrado com sucesso!\n");
-}
-
-void listarProdutos(Produto cardapio[], int totalProdutos)
-{
-    if (totalProdutos == 0)
-    {
-        printf("Nenhum produto cadastrado.\n");
-        return;
-    }
-
-    printf("\n======= CARDAPIO =======\n");
-
-    for (int i = 0; i < totalProdutos; i++)
-    {
-        printf("%d - %s - R$ %.2f\n",
-               i + 1,
-               cardapio[i].nome,
-               cardapio[i].preco);
-    }
-}
-
-void atualizarProduto(Produto cardapio[], int totalProdutos)
-{
-    int codigo;
-
-    if (totalProdutos == 0)
-    {
-        printf("Nenhum produto cadastrado.\n");
-        return;
-    }
-
-    listarProdutos(cardapio, totalProdutos);
-
-    printf("Digite o codigo do produto: ");
-    scanf("%d", &codigo);
-
-    if (codigo < 1 || codigo > totalProdutos)
-    {
-        printf("Codigo invalido!\n");
-        return;
-    }
-
-    printf("Novo nome: ");
-    scanf(" %[^\n]", cardapio[codigo - 1].nome);
-
-    printf("Novo preco: ");
-    scanf("%f", &cardapio[codigo - 1].preco);
-
-    if (cardapio[codigo - 1].preco < 0)
-    {
-        printf("Preco invalido!\n");
-        return;
-    }
-
-    printf("Produto atualizado com sucesso!\n");
-}
-
-void removerProduto(Produto cardapio[], int *totalProdutos)
-{
-    int codigo;
-
-    if (*totalProdutos == 0)
-    {
-        printf("Nenhum produto cadastrado.\n");
-        return;
-    }
-
-    listarProdutos(cardapio, *totalProdutos);
-
-    printf("Digite o codigo para remover: ");
-    scanf("%d", &codigo);
-
-    if (codigo < 1 || codigo > *totalProdutos)
-    {
-        printf("Codigo invalido!\n");
-        return;
-    }
-
-    for (int i = codigo - 1; i < *totalProdutos - 1; i++)
-    {
-        cardapio[i] = cardapio[i + 1];
-    }
-
-    (*totalProdutos)--;
-
-    printf("Produto removido com sucesso!\n");
-}
+#endif
