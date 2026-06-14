65584cba7c4df0a1e4997dff6e5b88b9ab1b785a
Barbaraa-tech <barbara.jesus@alunos.ufersa.edu.br>
2026-06-13 22:36:20 -0300

    Create produtos.c

---
+++ produtos.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include "produtos.h"
+
+Produto cardapio[MAX];
+int totalProdutos = 0;
+
+void adicionarProduto(void)
+{
+    if (totalProdutos >= MAX)
+    {
+        printf("Limite de produtos atingido.\n");
+        return;
+    }
+
+    printf("Digite o nome do produto: ");
+    scanf(" %[^\n]", cardapio[totalProdutos].nome);
+
+    printf("Digite o preco do produto: ");
+    scanf("%f", &cardapio[totalProdutos].preco);
+
+    totalProdutos++;
+
+    printf("Produto cadastrado com sucesso!\n");
+}
+
+void listarProdutos(void)
+{
+    if (totalProdutos == 0)
+    {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
+
+    printf("\n===== CARDAPIO =====\n");
+
+    for (int i = 0; i < totalProdutos; i++)
+    {
+        printf("%d - %s - R$ %.2f\n",
+               i + 1,
+               cardapio[i].nome,
+               cardapio[i].preco);
+    }
+}
+
+void atualizarProduto(void)
+{
+    int codigo;
+
+    listarProdutos();
+
+    printf("Digite o codigo do produto que deseja alterar: ");
+    scanf("%d", &codigo);
+
+    if (codigo < 1 || codigo > totalProdutos)
+    {
+        printf("Produto invalido!\n");
+        return;
+    }
+
+    printf("Novo nome: ");
+    scanf(" %[^\n]", cardapio[codigo - 1].nome);
+
+    printf("Novo preco: ");
+    scanf("%f", &cardapio[codigo - 1].preco);
+
+    printf("Produto atualizado com sucesso!\n");
+}
+
+void removerProduto(void)
+{
+    int codigo;
+
+    listarProdutos();
+
+    printf("Digite o codigo do produto que deseja remover: ");
+    scanf("%d", &codigo);
+
+    if (codigo < 1 || codigo > totalProdutos)
+    {
+        printf("Produto invalido!\n");
+        return;
+    }
+
+    for (int i = codigo - 1; i < totalProdutos - 1; i++)
+    {
+        cardapio[i] = cardapio[i + 1];
+    }
+
+    totalProdutos--;
+
+    printf("Produto removido com sucesso!\n");
+}
