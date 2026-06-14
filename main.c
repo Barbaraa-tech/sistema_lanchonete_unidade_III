b83816c752d60a427ff57ff8996ded610f3a8efd
Barbaraa-tech <barbara.jesus@alunos.ufersa.edu.br>
2026-06-13 21:28:25 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+/**********************************************************************
+* Componente Curricular: Algoritmos/Laboratório de Algoritmos
+* Projeto Final: Sistema de Lanchonete
+* Turma: PET2018 - Laboratório de Algoritmos - T01 (2026.1)
+*
+* Matrículas, Integrantes e Responsabilidades:
+*
+* 1. 2025022031 - Barbara Aquino de Jesus
+*    Responsável por: Cadastro e listagem de produtos
+*
+* 2. 2023023111 - Clayton dos Santos Bernardino
+*    Responsável por: Atualização e remoção de produtos
+*
+* 3. __________ - Francisca Mirelly Bezerra Pessoa
+*    Responsável por: Validação de dados e testes
+*
+* 4. __________ - Fabio Luan Pereira Melo
+*    Responsável por: Funções auxiliares e organização do sistema
+*
+* 5. __________ - Rafael Caetano Fernandes da Silva
+*    Responsável por: Documentação, README e vídeo explicativo
+*
+**********************************************************************/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, ".UTF8");
+
+    printf("=== SISTEMA DE LANCHONETE ===\n");
+
+    return 0;
+}
