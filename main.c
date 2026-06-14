eb32be262b259058a124d15244f48cfbff73aedd
Barbaraa-tech <barbara.jesus@alunos.ufersa.edu.br>
2026-06-13 22:58:21 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,66 +1,23 @@
 /**********************************************************************
-* Componente Curricular: Laboratório de Algoritmos
-* Projeto: Sistema de Lanchonete
-* Turma: PET2018 - T01 (2026.1)
+* Componente Curricular: Algoritmos/Laboratório de Algoritmos
+* Projeto Final: Sistema de Lanchonete
+* Turma: PET2018 - Laboratório de Algoritmos - T01 (2026.1)
 *
-* Integrantes:
-* - Barbara Aquino de Jesus
-* - Francisca Mirelly Bezerra Pessoa
-* - Clayton dos Santos Bernardino
-* - Fabio Luan Pereira Melo
-* - Rafael Caetano Fernandes da Silva
+* Matrículas, Integrantes e Responsabilidades:
+*
+* 1. 2025022019 - Francisca Mirelly Bezerra Pessoa
+*    Responsável por: Validação de dados e testes
+*
+* 2. 2025022031 - Barbara Aquino de Jesus
+*    Responsável por: Cadastro e listagem de produtos
+*
+* 3. 2023023111 - Clayton dos Santos Bernardino
+*    Responsável por: Atualização e remoção de produtos
+*
+* 4. 2025021998 - Fabio Luan Pereira Melo
+*    Responsável por: Funções auxiliares e organização do sistema
+*
+* 5. 2025010766 - Rafael Caetano Fernandes da Silva
+*    Responsável por: Documentação, README e vídeo explicativo
+*
 **********************************************************************/
-
-#include <stdio.h>
-#include <locale.h>
-#include "produtos.h"
-
-int main(void)
-{
-    setlocale(LC_ALL, ".UTF8");
-
-    int opcao;
-
-    do
-    {
-        printf("\n=================================\n");
-        printf("      SISTEMA DE LANCHONETE      \n");
-        printf("=================================\n");
-        printf("1 - Cadastrar produto\n");
-        printf("2 - Listar produtos\n");
-        printf("3 - Atualizar produto\n");
-        printf("4 - Remover produto\n");
-        printf("0 - Sair\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
-
-        switch (opcao)
-        {
-            case 1:
-                adicionarProduto();
-                break;
-
-            case 2:
-                listarProdutos();
-                break;
-
-            case 3:
-                atualizarProduto();
-                break;
-
-            case 4:
-                removerProduto();
-                break;
-
-            case 0:
-                printf("Encerrando o sistema...\n");
-                break;
-
-            default:
-                printf("Opcao invalida! Tente novamente.\n");
-        }
-
-    } while (opcao != 0);
-
-    return 0;
-}
